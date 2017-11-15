6fe436859f8ae31a550bda0fe2c140147e55ea76
Jonathan Lebon <jlebon@redhat.com>
2017-11-15 20:01:26 +0000

    papr: auto-detect image tag to use and bump to f27
    Current PAPR tests are failing because we're using an outdated image
    tag. Switch to auto-magically determining which image tag to use based
    on the branch the PR we're running in is targeting. This allows us to
    not have to worry about the tag version becoming stale again and is
    similar to what the tests from `aos-cd-jobs` are doing.
    
    Also bump to the brand new f27 release, which allows us to drop the
    temporary workaround for the glibc/libsolv bug.

--- .papr.sh
+++ .papr.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 set -xeuo pipefail
 
+pip install requests
+
+query_github() {
+    repo=$1; shift
+    resource=$1; shift
+    python -c "
+import sys
+import requests
+j = requests.get('https://api.github.com/repos/$repo/$resource').json()
+for q in sys.argv[1:]:
+    if q.isdigit():
+        q = int(q)
+    j = j[q]
+print(j)" "$@"
+}
+
+# Essentially use a similar procedure other openshift-ansible PR tests use to
+# determine which image tag should be used. This allows us to avoid hardcoding a
+# specific version which quickly becomes stale.
+
+if [ -n "${PAPR_BRANCH:-}" ]; then
+    target_branch=$PAPR_BRANCH
+else
+    # check which branch we're targeting if we're a PR
+    target_branch=$(query_github $PAPR_REPO pulls/$PAPR_PULL_ID base ref)
+    [ -n "$target_branch" ]
+fi
+
+# this is a bit wasteful, though there's no easy way to say "only clone up to
+# the first tag in the branch" -- ideally, PAPR could help with caching here
+git clone --branch $target_branch --single-branch https://github.com/openshift/origin
+export OPENSHIFT_IMAGE_TAG=$(git -C origin describe --abbrev=0)
+
 echo "Targeting OpenShift Origin $OPENSHIFT_IMAGE_TAG"
 
 pip install -r requirements.txt
@@ -27,7 +60,7 @@ ansible-playbook -vvv -i .papr.inventory playbooks/byo/config.yml -e "openshift_
 # be in a different OSP network
 ssh ocp-master docker run --rm --net=host --privileged \
   -v /etc/origin/master/admin.kubeconfig:/config \
-  registry.fedoraproject.org/fedora:26 sh -c \
+  registry.fedoraproject.org/fedora:27 sh -c \
     '"dnf install -y origin-tests && \
       KUBECONFIG=/config /usr/libexec/origin/extended.test --ginkgo.v=1 \
         --ginkgo.noColor --ginkgo.focus=\"Services.*NodePort|EmptyDir\""'
